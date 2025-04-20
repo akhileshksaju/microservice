7ed1b265aae905111e4c184af95dc99da850e196
akhileshksaju <akhilesh.saju1997@gmail.com>
2025-04-20 16:10:22 -0400

    security through keycloak

---
+++ api-gateway/src/main/java/com/aksprojects/Config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.aksprojects.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
+
+    serverHttpSecurity.csrf(t -> t.disable())
+                      .authorizeExchange(exchange -> exchange
+                        .pathMatchers("eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                        .oauth2ResourceServer(oauth2 ->oauth2.jwt());
+    return serverHttpSecurity.build();
+  }
+
+}
